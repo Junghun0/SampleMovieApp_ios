89440b4046142b891f46e5325e0a20aa8436ff67
parkjunghun <go9018@naver.com>
2019-01-31 15:56:04 +0900

    Tab Bar Controller 추가

---
+++ SampleMovieApp/TheaterCell.swift
@@ -0,0 +1,19 @@
+//
+//  TheaterCell.swift
+//  SampleMovieApp
+//
+//  Created by 박정훈 on 31/01/2019.
+//  Copyright © 2019 swift. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TheaterCell: UITableViewCell{
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var addr: UILabel!
+    
+    @IBOutlet weak var tel: UILabel!
+}
